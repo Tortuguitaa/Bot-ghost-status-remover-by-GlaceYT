8a1884b585419d4a905e4a23244ac2830ab56fe4
Tortuguitaa <147955839+Tortuguitaa@users.noreply.github.com>
2024-10-31 01:08:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Ouvindo Zeus Apostas","Jogando Zeus Apostas"];
 
 
 let currentIndex = 0;
